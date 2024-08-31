35a5b36cd973f621af1fcdb896254ec0cbe5552a
Vita <nikaangel295@gmail.com>
2024-08-31 11:27:31 +0100

    add

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import './LoginForm.css';
 import { FaUser, FaLock } from "react-icons/fa";
+import { useDispatch } from 'react-redux';
+import { logIn } from '../../redux/auth/operations';
 
 const LoginForm = () => {
+    const dispatch = useDispatch()
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+
+    dispatch(
+      logIn({
+        email: form.elements.email.value,
+        password: form.elements.password.value,
+      })
+    )
+      .unwrap()
+      .then(() => {
+        console.log("login success");
+      })
+      .catch(() => {
+        console.log("login error");
+      });
+
+    form.reset();
+  };
     return (
         <div className='wrapper'>
-            <form action="">
+            <form onSubmit={handleSubmit } action="">
                 <h1>Login</h1>
                 <div className="input-box">
                     <input type="text" placeholder='Username' required />
